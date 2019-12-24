7c43d249cc1273b8ae797c1e40c30b235b4e9f24
464716 <diesel.kg@mail.ru>
2019-12-24 13:59:24 +0600

    Изменение моделей

--- source/accounts/admin.py
+++ source/accounts/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import Profile
+from .models import Passport
 
-# Register your models here.
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    fields = ['birth_date', 'avatar']
+
+
+class ProfileAdmin(UserAdmin):
+    inlines = [ProfileInline]
+
+
+admin.site.unregister(User)
+admin.site.register(User, ProfileAdmin)
