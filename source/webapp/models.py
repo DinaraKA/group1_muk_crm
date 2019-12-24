156abeb5a65cd853f516f291b36f9f747b6fe2e3
Айсулу <aisuluueco2009@yandex.ru>
2019-12-24 13:19:42 +0600

    Слияние

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+
 class News(models.Model):
     title = models.CharField(max_length=200, null=False, blank=False, verbose_name='Заголовок')
     text = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Текст')
