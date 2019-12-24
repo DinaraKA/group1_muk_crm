91941df685476c9b73ac313747ff1afd45e89f0b
464716 <diesel.kg@mail.ru>
2019-12-24 14:04:13 +0600

    Снес к чертям модели

--- source/accounts/admin.py
+++ source/accounts/admin.py
@@ -1,18 +1,3 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import User
-from .models import Profile
-from .models import Passport
-
-
-class ProfileInline(admin.StackedInline):
-    model = Profile
-    fields = ['birth_date', 'avatar']
-
-
-class ProfileAdmin(UserAdmin):
-    inlines = [ProfileInline]
-
-
-admin.site.unregister(User)
-admin.site.register(User, ProfileAdmin)
