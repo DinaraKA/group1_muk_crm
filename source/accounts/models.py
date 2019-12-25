e45af86ba7f9589a0b9359cd317ab0ce0ca807b4
karamoldoevee <karamoldoevee@gmail.com>
2019-12-25 20:05:59 +0600

    Расширена модель User

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from phone_field import PhoneField
 
 
 class Position(models.Model):
@@ -14,3 +15,13 @@ class Parent(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile', verbose_name='Пользователь')
+    patronymic = models.CharField(max_length=30, null=True, blank=True, verbose_name='Отчество')
+    phone_number = PhoneField(null=True, blank=True, verbose_name='Номер телеофона')
+    photo = models.ImageField(null=True, blank=True, upload_to='', verbose_name='Фото')
+    address_fact = models.CharField(max_length=100, verbose_name='Фактический Адрес')
+    parent_one = models.OneToOneField(User, related_name='profile', verbose_name='Родитель Один')
+    parent_two = models.OneToOneField(User, related_name='profile', verbose_name='Родитель Два')
