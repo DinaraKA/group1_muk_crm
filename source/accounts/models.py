092334c80733dc621c72290fb5189dfaee7838ed
karamoldoevee <karamoldoevee@gmail.com>
2019-12-26 12:26:47 +0600

    Убраны старые модели

--- source/accounts/models.py
+++ source/accounts/models.py
@@ -3,13 +3,6 @@ from django.db import models
 from phone_field import PhoneField
 
 
-class Parent(models.Model):
-    user = models.ForeignKey(User, related_name='parents', on_delete=models.CASCADE, verbose_name='Родитель')
-
-    def __str__(self):
-        return str(self.id)
-
-
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile', verbose_name='Пользователь')
     patronymic = models.CharField(max_length=30, null=True, blank=True, verbose_name='Отчество')
