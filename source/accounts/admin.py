94ba51d1f235d8002f484cb150b5c4c61cfe79f1
karamoldoevee <karamoldoevee@gmail.com>
2019-12-26 14:07:19 +0600

    Созданы модели Соц.Статусов

--- source/accounts/admin.py
+++ source/accounts/admin.py
@@ -1,10 +1,7 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from django.contrib.auth.models import User
 from accounts.models import Profile, Passport, AdminPosition, UserAdminPosition, Role, UserRole
-#
-# admin.site.register(User)
-# admin.site.register(UserAdmin)
+
+
 admin.site.register(Profile)
 admin.site.register(Passport)
 admin.site.register(AdminPosition)
--- source/accounts/models.py
+++ source/accounts/models.py
@@ -59,3 +59,18 @@ class UserRole(models.Model):
 
     def __str__(self):
         return self.user.get_full_name()
+
+
+class SocialStatus(models.Model):
+    name = models.CharField(max_length=500, verbose_name='Статус')
+
+    def __str__(self):
+        return self.name
+
+
+class UserSocialStatus(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='socstatus', verbose_name='Пользователь')
+    role = models.ForeignKey('Role', on_delete=models.CASCADE, related_name='socstatus', verbose_name='Cтатус')
+
+    def __str__(self):
+        return self.user.get_full_name()
