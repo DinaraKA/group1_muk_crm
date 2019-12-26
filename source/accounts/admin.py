b1fbb9a7e2a83b7d34e860b6fcf92a581a22ddf7
karamoldoevee <karamoldoevee@gmail.com>
2019-12-26 14:08:47 +0600

    Зарегистрирована модель SocialStatus

--- source/accounts/admin.py
+++ source/accounts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from accounts.models import Profile, Passport, AdminPosition, UserAdminPosition, Role, UserRole
+from accounts.models import Profile, Passport, AdminPosition, UserAdminPosition, Role, UserRole, SocialStatus, \
+    UserSocialStatus
 
 
 admin.site.register(Profile)
@@ -8,3 +9,5 @@ admin.site.register(AdminPosition)
 admin.site.register(UserAdminPosition)
 admin.site.register(Role)
 admin.site.register(UserRole)
+admin.site.register(SocialStatus)
+admin.site.register(UserSocialStatus)
