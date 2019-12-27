d6aa4f12a52a11c1aaa6563e2447d9a2d5c8b58e
DinaraKA <d.karybekova@gmail.com>
2019-12-27 12:45:24 +0600

    Добавление страницы для отделения Связи(шаблон, переход по ссылке с главной страницы)

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, Department1View
 
 app_name = 'webapp'
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('departments/department1', Department1View.as_view(),  name='department1')
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -5,4 +5,7 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
     template_name = 'index.html'
 
+class Department1View(TemplateView):
+    template_name = 'departments/department1.html'
+
 
