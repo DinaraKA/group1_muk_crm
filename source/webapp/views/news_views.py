c14b1aab32d8f8cc2c0afaeebcca3062e23f4720
Айсулу <aisuluueco2009@yandex.ru>
2019-12-27 13:47:05 +0600

    Создание пагинации на странице новостей, для проверки пагинации для 2 новостей, по тикету 5 новостей на странице.

--- source/webapp/views/news_views.py
+++ source/webapp/views/news_views.py
@@ -7,6 +7,8 @@ class NewsView(ListView):
     template_name = 'news/all_news.html'
     context_object_name = 'news_list'
     ordering = ['-created_at']
+    paginate_by = 2
+    paginate_orphans = 0
 
 
 class NewsDetailView(DetailView):
