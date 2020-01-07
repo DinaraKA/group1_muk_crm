dabb54c9aab7b79e5727aae52700f69ab4bfe0fa
karamoldoevee <karamoldoevee@gmail.com>
2020-01-07 14:29:30 +0600

    Создание представления и шаблонов для редактирования 'AdminPosition'.

--- source/accounts/urls.py
+++ source/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from accounts.views.views import register_view
-from accounts.views.admin_position import AdminPositionCreateView
+from accounts.views.admin_position import AdminPositionCreateView, AdminPositionUpdateView
 
 app_name = 'accounts'
 
@@ -9,6 +9,6 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('create/', register_view, name='create'),
-    path('add_admin_position/', AdminPositionCreateView.as_view(), name='add_admin_position')
-
+    path('add_admin_position/', AdminPositionCreateView.as_view(), name='add_admin_position'),
+    path('change_admin_position/<int:pk>/', AdminPositionUpdateView.as_view(), name='change_admin_position')
 ]
--- source/accounts/views/admin_position.py
+++ source/accounts/views/admin_position.py
@@ -5,10 +5,18 @@ from django.urls import reverse, reverse_lazy
 from django.views.generic import CreateView, UpdateView, DeleteView
 
 
-
 class AdminPositionCreateView(CreateView):
     model = AdminPosition
     template_name = 'add.html'
+    fields = ['name']
+
+    def get_success_url(self):
+        return reverse('webapp:index')
+
+
+class AdminPositionUpdateView(UpdateView):
+    model = AdminPosition
+    template_name = 'change.html'
     fields = ['name']
 
     def get_success_url(self):
