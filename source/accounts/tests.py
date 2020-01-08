27e92dfe1b19aa8d15109506f0557372ce28c367
karamoldoevee <karamoldoevee@gmail.com>
2020-01-08 20:27:01 +0600

    Тест теперь на Selenium

--- source/accounts/tests.py
+++ source/accounts/tests.py
@@ -1,37 +1,34 @@
-from django.contrib.auth.models import User
+from webbrowser import Chrome
 from django.test import TestCase
-from django.urls import reverse
+from selenium.webdriver import Chrome
 
 
-class LoginLogoutViewTest(TestCase):
+class LoginTest(TestCase):
     def setUp(self):
-        test_user1 = User.objects.create_user(username='user', password='user')
+        self.driver = Chrome()
 
-        test_user1.save()
+    def tearDown(self):
+        self.driver.close()
 
-    def test_logged_in_uses_correct_template(self):
-        login = self.client.login(username='user', password='user')
-        response = self.client.get(reverse('webapp:index'))
+    def test_log_in_as_admin(self):
+        self.driver.get('http://localhost:8000/accounts/login/')
+        self.driver.find_element_by_name('username').send_keys('admin')
+        self.driver.find_element_by_name('password').send_keys('admin')
+        self.driver.find_element_by_css_selector('button').click()
+        assert self.driver.current_url == 'http://localhost:8000/'
 
-        self.assertEqual(str(response.context['user']), 'user')
 
-        self.assertEqual(response.status_code, 200)
+class LogoutTest(TestCase):
+    def setUp(self):
+        self.driver = Chrome()
 
-        self.assertTemplateUsed(response, 'index.html')
+    def tearDown(self):
+        self.driver.close()
 
-    def test_logged_out_uses_correct_template(self):
-        login = self.client.login(username='user', password='user')
-        response = self.client.get(reverse('webapp:index'))
-
-        self.assertEqual(str(response.context['user']), 'user')
-
-        self.assertEqual(response.status_code, 200)
-
-        self.assertTemplateUsed(response, 'index.html')
-
-        logout = self.client.logout()
-        response = self.client.get(reverse('webapp:index'))
-
-        self.assertEqual(response.status_code, 200)
-
-        self.assertTemplateUsed(response, 'index.html')
+    def test_logout_as_admin(self):
+        self.driver.get('http://localhost:8000/accounts/login/')
+        self.driver.find_element_by_name('username').send_keys('admin')
+        self.driver.find_element_by_name('password').send_keys('admin')
+        self.driver.find_element_by_css_selector('button').click()
+        self.driver.get('http://localhost:8000/accounts/logout/')
+        assert self.driver.current_url == 'http://localhost:8000/'
