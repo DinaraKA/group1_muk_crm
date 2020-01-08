989ebdfd0f257e99353e329aa9dc1ca9abfbc4d2
Айсулу <aisuluueco2009@yandex.ru>
2020-01-08 14:41:54 +0600

    #42.Создание страницы добавления новости для администратора.

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import IndexView, Department1View, Department2View, Department3View
-from .views import NewsDetailView, NewsView
+from .views import NewsDetailView, NewsView, NewsAddView
 
 app_name = 'webapp'
 
@@ -11,4 +11,5 @@ urlpatterns = [
     path('departments/department3/', Department3View.as_view(),  name='department3'),
     path('news/', NewsView.as_view(), name='news'),
     path('news/<int:pk>', NewsDetailView.as_view(), name='new_detail'),
+    path('news/add/', NewsAddView.as_view(), name='news_add'),
 ]
--- source/webapp/views/news_views.py
+++ source/webapp/views/news_views.py
@@ -1,4 +1,7 @@
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
+from django.urls import reverse_lazy, reverse
+from django.views.generic import ListView, DetailView, CreateView
 from webapp.models import News
 
 
@@ -15,3 +18,18 @@ class NewsDetailView(DetailView):
     model = News
     template_name = 'news/detail.html'
     context_object_name = 'news'
+
+
+class NewsAddView(LoginRequiredMixin, CreateView):
+    model = News
+    template_name = 'news/add.html'
+    fields = ('title', 'text', 'photo')
+    success_url = reverse_lazy('webapp:news')
+    permission_required = 'webapp.add_news'
+    permission_denied_message = "Доступ запрещен"
+
+
+    def get_success_url(self):
+        return reverse('webapp:new_detail', kwargs={'pk': self.object.pk})
+
+
