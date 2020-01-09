7426a4df541faa8a4a80764cb5f3646175d2594c
Айсулу <aisuluueco2009@yandex.ru>
2020-01-09 12:36:03 +0600

    #44.Создание страницы удаления новости.

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import IndexView, Department1View, Department2View, Department3View
-from .views import NewsDetailView, NewsView, NewsAddView, NewsEditView
+from .views import NewsDetailView, NewsView, NewsAddView, NewsEditView, NewsDeleteView
 
 app_name = 'webapp'
 
@@ -10,7 +10,9 @@ urlpatterns = [
     path('departments/department2/', Department2View.as_view(),  name='department2'),
     path('departments/department3/', Department3View.as_view(),  name='department3'),
     path('news/', NewsView.as_view(), name='news'),
-    path('news/<int:pk>', NewsDetailView.as_view(), name='new_detail'),
+    path('news/<int:pk>', NewsDetailView.as_view(), name='news_detail'),
     path('news/add/', NewsAddView.as_view(), name='news_add'),
     path('news/<int:pk>/edit/', NewsEditView.as_view(), name='news_edit'),
+    path('news/<int:pk>/delete/', NewsDeleteView.as_view(), name='news_delete'),
+
 ]
--- source/webapp/views/news_views.py
+++ source/webapp/views/news_views.py
@@ -1,7 +1,5 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import redirect
 from django.urls import reverse_lazy, reverse
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from webapp.models import News
 
 
@@ -10,37 +8,42 @@ class NewsView(ListView):
     template_name = 'news/all_news.html'
     context_object_name = 'news_list'
     ordering = ['-created_at']
-    paginate_by = 2
+    paginate_by = 1
     paginate_orphans = 0
 
 
 class NewsDetailView(DetailView):
     model = News
-    template_name = 'news/detail.html'
+    template_name = 'news/news_detail.html'
     context_object_name = 'news'
 
 
-class NewsAddView(LoginRequiredMixin, CreateView):
+class NewsAddView(CreateView):
     model = News
     template_name = 'news/add.html'
     fields = ('title', 'text', 'photo')
     success_url = reverse_lazy('webapp:news')
-    permission_required = 'webapp.add_news'
-    permission_denied_message = "Доступ запрещен"
 
     def get_success_url(self):
-        return reverse('webapp:new_detail', kwargs={'pk': self.object.pk})
+        return reverse('webapp:news_detail', kwargs={'pk': self.object.pk})
 
 
-class NewsEditView(LoginRequiredMixin, UpdateView):
+class NewsEditView(UpdateView):
     template_name = 'news/edit.html'
     model = News
     fields = ('title', 'text', 'photo')
     context_object_name = 'news'
-    permission_required = 'webapp.change_news'
-    permission_denied_message = 'Доступ запрещен!'
 
     def get_success_url(self):
-        return reverse('webapp:new_detail', kwargs={'pk': self.object.pk})
+        return reverse('webapp:news_detail', kwargs={'pk': self.object.pk})
+
+
+class NewsDeleteView(DeleteView):
+    model = News
+    template_name = 'news/delete.html'
+    context_object_name = 'news'
+    success_url = reverse_lazy('webapp:news')
+
+
 
 
