fd23ebed45c0629e11c99a5b62f388cd64161e83
karamoldoevee <karamoldoevee@gmail.com>
2020-01-13 12:00:32 +0600

    Добавлены старые тесты.

--- source/accounts/tests.py
+++ source/accounts/tests.py
@@ -116,6 +116,42 @@ class AdminPositionViewTest(TestCase):
         self.driver.find_element_by_class_name('btn.btn-danger').click()
         assert self.driver.current_url == 'http://127.0.0.1:8000/'
 
+
+    # class LoginLogoutViewTest(TestCase):
+    #     def setUp(self):
+    #         test_user1 = User.objects.create_user(username='user', password='user')
+    #
+    #         test_user1.save()
+    #
+    #     def test_logged_in_uses_correct_template(self):
+    #         login = self.client.login(username='user', password='user')
+    #         response = self.client.get(reverse('webapp:index'))
+    #
+    #         self.assertEqual(str(response.context['user']), 'user')
+    #
+    #         self.assertEqual(response.status_code, 200)
+    #
+    #         self.assertTemplateUsed(response, 'index.html')
+    #
+    #     def test_logged_out_uses_correct_template(self):
+    #         login = self.client.login(username='user', password='user')
+    #         response = self.client.get(reverse('webapp:index'))
+    #
+    #         self.assertEqual(str(response.context['user']), 'user')
+    #
+    #         self.assertEqual(response.status_code, 200)
+    #
+    #         self.assertTemplateUsed(response, 'index.html')
+    #
+    #         logout = self.client.logout()
+    #         response = self.client.get(reverse('webapp:index'))
+    #
+    #         self.assertEqual(response.status_code, 200)
+    #
+    #         self.assertTemplateUsed(response, 'index.html')
+
+
+    # class AdminPositionViewTest(TestCase):
     # def test_created_position(self):
     # response = self.client.post(reverse('accounts:add_admin_position'), {'name': 'test'})
     # self.assertEqual(AdminPosition.objects.get().name, 'test')
