0a3868db5441ae99bc4a032870d89584ea54db9e
tyzo4ka <tyzo4ka@gmail.com>
2020-01-15 14:47:48 +0600

    Исправлен тест LoginTest

--- source/accounts/tests/user_tests.py
+++ source/accounts/tests/user_tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver import Chrome
 
 
@@ -11,11 +12,15 @@ class LoginTest(TestCase):
 
     def test_log_in_as_admin(self):
         self.driver.get('http://127.0.0.1:8000/accounts/login/')
-        # self.driver.find_element_by_class_name('login').click()
         self.driver.find_element_by_name('username').send_keys('admin')
         self.driver.find_element_by_name('password').send_keys('admin')
         self.driver.find_element_by_css_selector('button[type="submit"]').click()
         assert self.driver.current_url == 'http://127.0.0.1:8000/'
+        try:
+            link = self.driver.find_element_by_link_text("Привет, admin!")
+        except NoSuchElementException:
+            link = None
+        assert link is not None
 
 
 class LogoutTest(TestCase):
