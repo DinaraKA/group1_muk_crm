4826c18e152d947b307c5862765bcd7aa5d42a51
karamoldoevee <karamoldoevee@gmail.com>
2020-01-22 12:53:00 +0600

    Добавлены тестовые данные для тем в базе данных.

--- source/accounts/admin.py
+++ source/accounts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from accounts.models import User, Profile, Passport, AdminPosition, UserAdminPosition, Role, UserRole, SocialStatus, \
-    Status, Group
+    Status, Group, Theme
 from django.contrib.auth.admin import UserAdmin
 
 
@@ -25,3 +25,4 @@ admin.site.register(UserRole)
 admin.site.register(SocialStatus)
 admin.site.register(Status)
 admin.site.register(Group)
+admin.site.register(Theme)
