9ab836dd10236015f1d3c49fdb99d1d99971a5ba
karamoldoevee <karamoldoevee@gmail.com>
2020-01-23 22:02:01 +0600

    Добавлена страница создания для Progress(Успеваемость.

---
+++ source/accounts/views/progress_views.py
@@ -0,0 +1,27 @@
+from accounts.models import Progress
+from django.urls import reverse, reverse_lazy
+from django.urls import reverse, reverse_lazy
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+
+
+class ProgressIndexView(ListView):
+    template_name = 'progress/list.html'
+    model = Progress
+    context_object_name = 'progress'
+    paginate_by = 30
+    paginate_orphans = 0
+    page_kwarg = 'page'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data()
+        return context
+
+
+class ProgressCreateView(CreateView):
+    model = Progress
+    template_name = 'add.html'
+    fields = ['student', 'date', 'discipline', 'theme', 'grade']
+
+    def get_success_url(self):
+        return reverse('accounts:progress]')
+
