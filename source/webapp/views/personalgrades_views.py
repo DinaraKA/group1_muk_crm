5f7e7d2fe477b81aeb1667d55db8ac893ef6a46a
tyzo4ka <tyzo4ka@gmail.com>
2020-01-29 18:59:00 +0600

    Изменено название Progress на Journal

--- source/webapp/views/personalgrades_views.py
+++ source/webapp/views/personalgrades_views.py
@@ -1,18 +1,18 @@
 from django.views.generic import ListView
 
-from accounts.models import Progress
+from webapp.models import Journal
 
 
 class PersonalGradesListView(ListView):
     template_name = 'personalgrades/personalgrades.html'
-    model = Progress
+    model = Journal
     ordering = ["discipline"]
     context_object_name = 'grades'
 
     def get_context_data(self, **kwargs):
         context = super(PersonalGradesListView, self).get_context_data(**kwargs)
         context.update({
-            'grades': Progress.objects.order_by('date')
+            'grades': Journal.objects.order_by('date')
         })
         return context
 
