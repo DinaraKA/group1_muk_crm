1f84da7672f70ca753561a2863e5b468189c93d4
tyzo4ka <tyzo4ka@gmail.com>
2020-01-30 09:17:56 +0600

    Удалено значение  default из index  модели Lesson

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -56,7 +56,7 @@ class Discipline(models.Model):
 
 
 class Lesson(models.Model):
-    index = models.IntegerField(verbose_name="Порядковый номер", default=1)
+    index = models.IntegerField(verbose_name="Порядковый номер")
     name = models.CharField(max_length=20, null=False, blank=False, verbose_name="Пара")
     start_time = models.TimeField(verbose_name="Время начала")
     end_time = models.TimeField(verbose_name="Время окончания")
@@ -72,7 +72,7 @@ class SaturdayLesson(models.Model):
     end_time = models.TimeField(verbose_name="Время окончания")
 
     def __str__(self):
-        return self.name
+        return str(self.index) + " пара"
 
 
 class Theme(models.Model):
