3d59d978e5682958c28178c03a19c01796cc4182
tyzo4ka <tyzo4ka@gmail.com>
2020-02-17 11:56:46 +0600

    Добавлены разрешения для создания, удаления, просмотра, изменения Групп

--- source/accounts/views/group_views.py
+++ source/accounts/views/group_views.py
@@ -1,45 +1,56 @@
+from django.contrib.auth.mixins import PermissionRequiredMixin
 from accounts.models import Group
 from django.urls import reverse, reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 
-class GroupListView(ListView):
+class GroupListView(PermissionRequiredMixin, ListView):
     template_name = 'group/list.html'
     model = Group
     ordering = ["-name"]
     context_object_name = 'groups'
     paginate_by = 10
     paginate_orphans = 2
+    permission_required = "accounts.view_group"
+    permission_denied_message = "Доступ запрещен"
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data()
         return context
 
 
-class GroupDetailView(DetailView):
+class GroupDetailView(PermissionRequiredMixin, DetailView):
     template_name = 'group/detail.html'
     model = Group
+    permission_required = "accounts.view_group"
+    permission_denied_message = "Доступ запрещен"
 
 
-class GroupCreateView(CreateView):
+class GroupCreateView(PermissionRequiredMixin, CreateView):
     model = Group
     template_name = 'add.html'
     fields = ['name', 'students', 'starosta', 'kurator', 'started_at']
+    permission_required = "accounts.add_group"
+    permission_denied_message = "Доступ запрещен"
 
     def get_success_url(self):
         return reverse('accounts:groups')
 
 
-class GroupUpdateView(UpdateView):
+class GroupUpdateView(PermissionRequiredMixin, UpdateView):
     model = Group
     template_name = 'change.html'
-    fields = ['name', 'students', 'starosta', 'kurator', 'started_at']  
+    fields = ['name', 'students', 'starosta', 'kurator', 'started_at']
+    permission_required = "accounts.change_group"
+    permission_denied_message = "Доступ запрещен"
 
     def get_success_url(self):
         return reverse('accounts:groups')
 
 
-class GroupDeleteView(DeleteView):
+class GroupDeleteView(PermissionRequiredMixin, DeleteView):
     model = Group
     template_name = 'delete.html'
     success_url = reverse_lazy('accounts:groups')
+    permission_required = "accounts.delete_group"
+    permission_denied_message = "Доступ запрещен"
