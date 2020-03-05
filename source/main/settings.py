385a3fd5762d018625137e901a05e0773b7f27ce
464716 <diesel.kg@mail.ru>
2020-03-06 03:14:27 +0600

    убрал кнопки создания, редактирования и удаления при просмотре сайта с телефонов Iphone, осталось вроверить на Android, выявил пару косяков, создал сообветствующие тикеты в Bugs

--- source/main/settings.py
+++ source/main/settings.py
@@ -25,9 +25,9 @@ SECRET_KEY = '9^mr=0j@i8i19p-py1+o#k_ro3zn61(gef^@2pnm7$2n29!u4q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'testserver', ]
-# ALLOWED_HOSTS = []
+#
+# ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'testserver', ]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
