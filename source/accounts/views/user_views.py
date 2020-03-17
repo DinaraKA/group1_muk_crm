e8456474157b1b27b7e78f194cd0db9e89cbb7ef
tyzo4ka <tyzo4ka@gmail.com>
2020-03-17 16:12:08 +0600

    Добавлена проверка на staff при показе бокового меню

--- source/accounts/views/user_views.py
+++ source/accounts/views/user_views.py
@@ -16,22 +16,6 @@ from django.utils.http import urlencode
 from django.db.models import Q
 from ajax_search.forms import SearchForm
 
-# from django import template
-# register = template.Library()
-#
-#
-# # @register.filter(name='is_group')
-# # def is_group(user, group_name):
-# #     try:
-# #         group = Group.objects.get(name=group_name)
-# #         return group in user.groups.all()
-# #     except Group.DoesNotExist:
-# #         return False
-#
-# @register.filter
-# def has_group(user, group_name):
-#     return user.groups.filter(name=group_name).exists()
-
 
 def login_view(request, *args, **kwargs):
     context = {}
