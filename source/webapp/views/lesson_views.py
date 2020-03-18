6b15c3a9f43b169277829d934634debac02b897f
tyzo4ka <tyzo4ka@gmail.com>
2020-03-18 13:51:28 +0600

    Добавлен UserpassesTestMixin к Crud модели Lesson. Только пользователи входящие в группу pricipal_staff имеют доступ

--- source/webapp/views/lesson_views.py
+++ source/webapp/views/lesson_views.py
@@ -1,12 +1,17 @@
+from django.contrib.auth.mixins import UserPassesTestMixin
 from webapp.models import Lesson
 from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 
 
-class LessonListView(ListView):
+class LessonListView(UserPassesTestMixin, ListView):
     template_name = 'lesson/all_lessons.html'
     model = Lesson
     ordering = ["start_time"]
+
+    def test_func(self):
+        user = self.request.user
+        return user.is_staff or user.groups.filter(name='principal_staff')
 
     def get_context_data(self, **kwargs):
         context = super(LessonListView, self).get_context_data(**kwargs)
@@ -19,21 +24,33 @@ class LessonListView(ListView):
         return context
 
 
-class LessonCreateView(CreateView):
+class LessonCreateView(UserPassesTestMixin, CreateView):
     model = Lesson
     template_name = 'add.html'
     fields = ['index', "is_saturday", "start_time", "end_time"]
     success_url = reverse_lazy('webapp:lessons')
 
+    def test_func(self):
+        user = self.request.user
+        return user.is_staff or user.groups.filter(name='principal_staff')
 
-class LessonUpdateView(UpdateView):
+
+class LessonUpdateView(UserPassesTestMixin, UpdateView):
     model = Lesson
     template_name = 'change.html'
     fields = ['index', "is_saturday", "start_time", "end_time"]
     success_url = reverse_lazy('webapp:lessons')
 
+    def test_func(self):
+        user = self.request.user
+        return user.is_staff or user.groups.filter(name='principal_staff')
 
-class LessonDeleteView(DeleteView):
+
+class LessonDeleteView(UserPassesTestMixin, DeleteView):
     model = Lesson
     template_name = 'delete.html'
     success_url = reverse_lazy('webapp:lessons')
+
+    def test_func(self):
+        user = self.request.user
+        return user.is_staff or user.groups.filter(name='principal_staff')
