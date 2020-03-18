7e2faca7129769bc02db8fe7efe060ca55c63dd6
tyzo4ka <tyzo4ka@gmail.com>
2020-03-18 16:45:10 +0600

    Изменен доступ к CRUD дисциплины. Только пользователи из учебной части имеют доступ

--- source/webapp/views/discipline_views.py
+++ source/webapp/views/discipline_views.py
@@ -1,28 +1,37 @@
+from django.contrib.auth.mixins import UserPassesTestMixin
+
 from webapp.forms import DisciplineForm
 from webapp.models import Discipline
 from django.urls import reverse, reverse_lazy
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView
-from django.shortcuts import redirect, get_object_or_404
+from django.shortcuts import redirect
 from django.contrib import messages
 from django.shortcuts import render
 
 
-class DisciplineListView(ListView):
+class DisciplineListView(UserPassesTestMixin, ListView):
     template_name = 'disciplines/disciplines.html'
     model = Discipline
     ordering = ["name"]
     context_object_name = 'disciplines'
 
+    def test_func(self):
+        user = self.request.user
+        return user.is_staff or user.groups.filter(name='principal_staff')
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data()
         return context
 
 
-class DisciplineCreateView(CreateView):
+class DisciplineCreateView(UserPassesTestMixin, CreateView):
     model = Discipline
     template_name = 'add.html'
     form_class = DisciplineForm
+
+    def test_func(self):
+        user = self.request.user
+        return user.is_staff or user.groups.filter(name='principal_staff')
 
     def form_valid(self, form):
         text = form.cleaned_data['name']
@@ -34,51 +43,30 @@ class DisciplineCreateView(CreateView):
             discipline = Discipline(name=text.capitalize())
             discipline.save()
             return self.get_success_url()
-        # # teacher = teacher_text.teacher()
-        # # print(i.last_name, "THIS IS I")
-        # print(Discipline.objects.filter(name='Тест'), 'THIS IS DISCIPLINE FILTER')
-        # obj, boolean = Discipline.objects.get_or_create(name=text)
-        #
-        # if boolean == False:
-        #     messages.error(self.request, 'Объект с таким названием уже существует!')
-        #     return render(self.request, 'add.html', {})
-        # else:
-        #     dis = Discipline.objects.get(name=text)
-        #     for i in teacher_text:
-        #         if i in dis.teacher.all():
-        #             print("false")
-        #         else:
-        #             print("true")
-        #             obj.teacher.add(i)
-        #     obj.save()
-        # return self.get_success_url()
 
     def get_success_url(self):
         return redirect('webapp:disciplines')
 
 
-class DisciplineUpdateView(UpdateView):
+class DisciplineUpdateView(UserPassesTestMixin, UpdateView):
     model = Discipline
     template_name = 'change.html'
     form_class = DisciplineForm
 
-    # def form_valid(self, form):
-    #     text = form.cleaned_data['name']
-    #     teacher_text = form.cleaned_data['teacher']
-    #     if Discipline.objects.filter(name=text.capitalize()):
-    #         messages.error(self.request, 'Объект с таким названием уже существует!')
-    #         return render(self.request, 'add.html', {})
-    #     else:
-    #         discipline = Discipline(name=text.capitalize())
-    #         discipline.save()
-    #         return self.get_success_url()
+    def test_func(self):
+        user = self.request.user
+        return user.is_staff or user.groups.filter(name='principal_staff')
 
     def get_success_url(self):
         return reverse('webapp:disciplines')
 
 
-class DisciplineDeleteView(DeleteView):
+class DisciplineDeleteView(UserPassesTestMixin, DeleteView):
     model = Discipline
     template_name = 'delete.html'
     success_url = reverse_lazy('webapp:disciplines')
+
+    def test_func(self):
+        user = self.request.user
+        return user.is_staff or user.groups.filter(name='principal_staff')
 
